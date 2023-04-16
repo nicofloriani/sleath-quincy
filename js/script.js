5bbe4fbc2ad38f147105ad9c46b60595a2eea4aa
nico_floriani <floriani.nico@gmail.com>
2023-04-16 15:24:27 -0700

    Added event handler for dark/light toggle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var fans = document.querySelector(".fans");
 var fish = document.querySelector(".fish");
 var pets = document.querySelector(".pets");
+var toggleView = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
 
 fans.addEventListener("mouseover", function () {
     fans.innerText = "123K";
@@ -12,4 +16,16 @@ fish.addEventListener("mouseover", function () {
 
 pets.addEventListener("mouseover", function () {
     pets.innerText = "20K";
+});
+
+toggleView.addEventListener("click", function () {
+    if (body.classList.contains("light")) {
+        body.classList.remove("light");
+        ball.classList.remove("move-right");
+        h1.innerText = "Sleath Quincy";
+    } else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+        h1.innerText = "Party Quincy";
+    }
 });
